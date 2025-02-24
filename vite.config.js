7c0e2a65c8d691f5e53d93175be69290df3dcbf1
q22641949@gmail.com <q22641949@gmail.com>
2025-02-25 01:01:04 +0800

    fix: modify base setting

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
+/* global process */
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/websocket-chatroom/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
